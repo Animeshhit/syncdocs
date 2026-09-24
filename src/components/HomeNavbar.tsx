79675099be16ef720c40b9bde326a9f9efe7add7
Animeshhit <animeshkumbhakarhit2@gmail.com>
2026-09-24 22:50:01 +0530

    fixed ui for mobile

--- src/components/HomeNavbar.tsx
+++ src/components/HomeNavbar.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 
 const HomeNavbar = () => {
   return (
-    <header className="px-5 py-3">
-      <div className="container mx-auto flex items-center justify-between">
-        <Link href="/">
-          <div className="relative w-50 h-12.5">
+    <header className="px-3 py-3 sm:px-5">
+      <div className="container mx-auto flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
+        <Link href="/" className="self-start sm:self-auto">
+          <div className="relative h-10 w-36 sm:h-12 sm:w-48 md:w-52">
             <Image
               loading="eager"
               src="/syncdocs-logo.svg"
@@ -17,14 +17,17 @@ const HomeNavbar = () => {
             />
           </div>
         </Link>
-        <div className="flex items-center gap-3">
-          <OrganizationSwitcher
-            afterLeaveOrganizationUrl="/"
-            afterSelectOrganizationUrl="/"
-            afterSelectPersonalUrl="/"
-            afterCreateOrganizationUrl="/"
-          />
-          <UserButton />
+
+        <div className="flex w-full items-center justify-end gap-2 sm:w-auto sm:gap-3">
+          <div className="flex items-center gap-2 sm:gap-3">
+            <OrganizationSwitcher
+              afterLeaveOrganizationUrl="/"
+              afterSelectOrganizationUrl="/"
+              afterSelectPersonalUrl="/"
+              afterCreateOrganizationUrl="/"
+            />
+            <UserButton />
+          </div>
         </div>
       </div>
     </header>
